24beada71fb92b833313003e25509106358de6f5
jmarcos00312 <jeremiahmarcos27@outlook.com>
2021-12-27 14:28:25 -0600

    edit sold and sale column to show false instead of null

--- db/migrate/20211227192658_add_default_value_to_sale_item.rb
+++ db/migrate/20211227192658_add_default_value_to_sale_item.rb
@@ -1,6 +1,7 @@
 class AddDefaultValueToSaleItem < ActiveRecord::Migration[6.1]
   def change
-    change_column :items, :sale, :boolean, :default => false
+    # change_column :items, :sale, :boolean, default: false
+    change_column_default :items, :sale, false
     #Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
 end
