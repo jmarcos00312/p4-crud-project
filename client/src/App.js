8465b5d9722ac69aae8858dd8cfb5fe79d8e7c34
DanielSheehan <daniel.sheehan81@yahoo.com>
2022-01-04 17:59:45 -0500

    Cleaned modules and updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,18 @@
-
 import './App.css';
-import React, { useState, useEffect } from "react";
-import Header from './Header';
+import Home from './components/Home'
+import { useEffect, useState } from "react"
+
 
 function App() {
-  const [shoes, setShoes] = useState([])
-
-  useEffect(() => {
-    fetch('/items', { credentials: "include",}).then(r => r.json()).then(data => setShoes(data))
-  }, [])
-  console.log(shoes)
-
-  let everyshoes = shoes.map(shoes => {
-    return (
-      <div>
-        <h1>{shoes.name}</h1>
-        <img src={shoes.img_url} />
-      </div>
-    )
-  })
-  // let movieList = movies.map(movie => {
-  //   return (
-  //     <div className='d-flex'>
-  //       <div className="row_poster">
-  //         <div class="hover-container">
-  //           <img src={movie.img_url} alt='movie' onClick={() => history.push("/movies")}></img>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   )
-  // })
+  const [currentUser, setCurrentUser] = useState(null)
+  const [authenticated, setAuthenticated] = useState(false)
+  console.log(currentUser)
 
   return (
       <>
-      <div>
-      <Header />
-      </div>
+     
     <div className="App">
-      {everyshoes}
-      {/* <h1>Hello world</h1> */}
+      <Home />
     </div>
     </>
   );
