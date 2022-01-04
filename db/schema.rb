93c1d462a72956bf8b794b475169e85cd1268c02
Jeremiah <jeremiahmarcos27@outlook.com>
2022-01-04 12:07:35 -0600

    redownload csv

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_223145) do
+ActiveRecord::Schema.define(version: 2022_01_03_162634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "branded_items", force: :cascade do |t|
-    t.bigint "brand_id", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["brand_id"], name: "index_branded_items_on_brand_id"
-    t.index ["item_id"], name: "index_branded_items_on_item_id"
-  end
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -32,12 +23,12 @@ ActiveRecord::Schema.define(version: 2022_01_03_223145) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.string "price"
+    t.float "price"
     t.float "rating"
     t.integer "seller_id"
     t.integer "buyer_id"
     t.bigint "brand_id", null: false
-    t.boolean "sold", default: false
+    t.boolean "sold"
     t.boolean "sale", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -53,22 +44,9 @@ ActiveRecord::Schema.define(version: 2022_01_03_223145) do
     t.string "img_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
     t.string "email"
   end
 
-  create_table "watch_lists", force: :cascade do |t|
-    t.bigint "item_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_watch_lists_on_item_id"
-    t.index ["user_id"], name: "index_watch_lists_on_user_id"
-  end
-
-  add_foreign_key "branded_items", "brands"
-  add_foreign_key "branded_items", "items"
   add_foreign_key "items", "brands"
-  add_foreign_key "watch_lists", "items"
-  add_foreign_key "watch_lists", "users"
 end
