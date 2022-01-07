344731c4f8d69178ae3fbfe0298e31750be99dba
Jeremiah <jeremiahmarcos27@outlook.com>
2022-01-07 09:20:01 -0600

    fixed brand form

--- client/src/components/NewShoes.js
+++ client/src/components/NewShoes.js
@@ -56,7 +56,7 @@ function NewShoes({ currentUser }) {
             {/* {console.log(id)} */}
             {/* {console.log(currentUser)} */}
             <h1>FORM HERE</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} id="shoe-form">
                 <h1>Create New Shoes</h1>
                 <label>
                     Shoe Name:
@@ -85,16 +85,20 @@ function NewShoes({ currentUser }) {
                         onChange={(e) => handleChange(e)}
                         required />
                 </label>
-                <label>Brand:
-                    <input
-                        name="brand_id"
-                        type="number"
-                        value={newShoeForm.brand_id}
-                        onChange={(e) => handleChange(e)}
-                        required />
-                </label>
+
+                <label for="brand_id">Brand:</label>
+                <select name="brand_id" id="brands" form="shoe-form" value={newShoeForm.brand_id} onChange={(e) => handleChange(e)}>
+                    <option value="1">Adidas</option>
+                    <option value="3">Air Jordan</option>
+                    <option value="4">Balenciaga</option>
+                    <option value="10">New Balance</option>
+                    <option value="11">Prada</option>
+                    <option value="9">Other</option>
+                </select>
+                <br />
                 <button>Submit</button>
             </form>
+
         </div>
     )
 }
